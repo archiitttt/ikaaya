de2e690dc8dfbf18f07c4c882fb90a35fe254767
Archit Kaushal <architkaushal2004@gmail.com>
2026-02-16 14:32:39 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import "./icons/fontawesome";
+import "./Icons/fontawesome";
 import './index.css'
 import {AuthProvider} from './Context/AuthContext.jsx'
-// import {CartProvider} from './Context/CartContext.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
